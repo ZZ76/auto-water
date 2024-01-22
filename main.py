9c4d7ea2f4e7e8d368d0e241461a53b345d9b55c
zz76 <zhuyuhong76@gmail.com>
2024-01-23 00:59:44 +1300

    fix error

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from watering_device import WateringDevice
 from aws.iot import mqtt_receiver
 import config
 import iot
+import logging
 
 logging.basicConfig(format='%(asctime)s-%(name)s-%(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
